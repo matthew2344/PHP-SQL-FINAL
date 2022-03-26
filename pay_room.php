6ef9150eff128904e496cfa0944e88e57bf67fa6
matthew2344 <57953122+matthew2344@users.noreply.github.com>
2022-03-26 15:36:00 +0800

    work-in-progress

--- pay_room.php
+++ pay_room.php
@@ -172,61 +172,9 @@ include('header.php');
     </div>
   </section>
 
-  <?php 
-   $slider = "SELECT * FROM slider";
-   $slider_run = mysqli_query($connection, $slider);
-   
-   if($slider_run)
-   {   
-     $imgSR = mysqli_fetch_assoc($slider_run);   
-     $imgF = $imgSR['image']; 
-?>
-  <section class="probootstrap-half">
-    <div class="image" style="background-image: url(admin/upload/<?php echo $imgF;?>);"></div>
-    <div class="text">
-      <div class="probootstrap-animate fadeInUp probootstrap-animated">
-        <h2 class="mt0">Best 5 Star hotel</h2>
-        <p><img src="img/curve_white.svg" class="seperator" alt="Free HTML5 Bootstrap Template"></p>
-        <div class="row">
-          <div class="col-md-6">
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>    
-          </div>
-          <div class="col-md-6">
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>    
-          </div>
-        </div>
-        <p><a href="#" class="link-with-icon white">Learn More <i class=" icon-chevron-right"></i></a></p>
-      </div>
-    </div>
-  </section>
-<?php
-  }
-  else
-  {
-?>
-  <section class="probootstrap-half">
-    <div class="image" style="background-image: url(img/slider_2.jpg);"></div>
-    <div class="text">
-      <div class="probootstrap-animate fadeInUp probootstrap-animated">
-        <h2 class="mt0">Best 5 Star hotel</h2>
-        <p><img src="img/curve_white.svg" class="seperator" alt="Free HTML5 Bootstrap Template"></p>
-        <div class="row">
-          <div class="col-md-6">
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>    
-          </div>
-          <div class="col-md-6">
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>    
-          </div>
-        </div>
-        <p><a href="#" class="link-with-icon white">Learn More <i class=" icon-chevron-right"></i></a></p>
-      </div>
-    </div>
-  </section>
-<?php 
-  }
-?>
 
 
+  <?php include('extra.php')?> 
 <?php
 include('footer.php');
 ?>
--- reservation.php
+++ reservation.php
@@ -134,70 +134,9 @@ include('header.php');
     }
   ?>
 
-  <?php 
-   $slider = "SELECT * FROM slider";
-   $slider_run = mysqli_query($connection, $slider);
-   
-   if($slider_run)
-   {
-      $slider_row = mysqli_fetch_assoc($slider_run);
-?>
-  <section class="probootstrap-half">
-    <div class="image" style="background-image: url(admin/upload/<?php echo $slider_row['image'];?>);"></div>
-    <div class="text">
-      <div class="probootstrap-animate fadeInUp probootstrap-animated">
-        <h2 class="mt0">Best 5 Star hotel</h2>
-        <p><img src="img/curve_white.svg" class="seperator" alt="Free HTML5 Bootstrap Template"></p>
-        <div class="row">
-          <div class="col-md-6">
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>    
-          </div>
-          <div class="col-md-6">
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>    
-          </div>
-        </div>
-        <p><a href="#" class="link-with-icon white">Learn More <i class=" icon-chevron-right"></i></a></p>
-      </div>
-    </div>
-  </section>
-<?php 
-  }
-  else
-  {
-?>
-  <section class="probootstrap-half">
-    <div class="image" style="background-image: url(img/slider_2.jpg);"></div>
-    <div class="text">
-      <div class="probootstrap-animate fadeInUp probootstrap-animated">
-        <h2 class="mt0">Best 5 Star hotel</h2>
-        <p><img src="img/curve_white.svg" class="seperator" alt="Free HTML5 Bootstrap Template"></p>
-        <div class="row">
-          <div class="col-md-6">
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>    
-          </div>
-          <div class="col-md-6">
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>    
-          </div>
-        </div>
-        <p><a href="#" class="link-with-icon white">Learn More <i class=" icon-chevron-right"></i></a></p>
-      </div>
-    </div>
-  </section>
-<?php 
-  }
-?>
 
 
-
-
-
-
-
-
-
-
-
-
+<?php include('extra.php')?> 
 
 
 
